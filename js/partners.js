f0900c439f1f7f13d69d5328d3acab9208441922
cliffgor <clierozigy@gmail.com>
2017-03-09 14:50:36 +0300

    modal working properly

--- js/partners.js
+++ js/partners.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
     $("#login-btn").click(function(event) {
         event.preventDefault();
+        $("#login-btn").click(function() {
+            $("#myModal").modal();
+        });
     });
 });
